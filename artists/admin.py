9105c3f8a98bfdbf1991c0cc24d7bc772b0e6f57
Mohammad <mohammad.hijazi.2000@gmailcom>
2022-08-17 17:42:11 +0300

    added models

--- artists/admin.py
+++ artists/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Artists)
+
+admin.site.register(Gender)
+
+admin.site.register(Countries)
--- artists/models.py
+++ artists/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class Artists(models.Model):
+    name = models.CharField(max_length=64, null=True)
+    age = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Gender(models.Model):
+    GENDER_CHOICES = (("M", "Male"), ("F", "Female"), ("O", "Other"))
+
+    artist = models.ForeignKey(Artists, on_delete=models.CASCADE)
+    gender = models.CharField(max_length=7, choices=GENDER_CHOICES)
+
+    def __str__(self):
+        return self.gender
+
+
+class Countries(models.Model):
+    COUNTRY_CHOICES = (
+        ("USA", "America"),
+        ("Fr", "France"),
+        ("UK", "United Kingdom"),
+        ("Sp", "Spain"),
+        ("Gr", "Germany"),
+    )
+
+    artist = models.ForeignKey(Artists, on_delete=models.CASCADE)
+    country = models.CharField(max_length=16, choices=COUNTRY_CHOICES)
+
+    def __str__(self):
+        return self.country
