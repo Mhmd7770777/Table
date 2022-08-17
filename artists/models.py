65a85ab49a4b9db214636713a1aa82afe31ca8fa
Mohammad <mohammad.hijazi.2000@gmailcom>
2022-08-17 18:57:23 +0300

    fix models

--- artists/models.py
+++ artists/models.py
@@ -1,18 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class Artist(models.Model):
-    name = models.CharField(max_length=64, null=True)
-    age = models.IntegerField(null=True)
-
-    def __str__(self):
-        return self.name
 
 
 class Gender(models.Model):
     GENDER_CHOICES = (("M", "Male"), ("F", "Female"), ("O", "Other"))
 
-    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     gender = models.CharField(max_length=7, choices=GENDER_CHOICES)
 
     def __str__(self):
@@ -28,8 +21,17 @@ class Country(models.Model):
         ("Gr", "Germany"),
     )
 
-    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     country = models.CharField(max_length=16, choices=COUNTRY_CHOICES)
 
     def __str__(self):
         return self.country
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=64, null=True)
+    age = models.IntegerField(null=True)
+    gender = models.ForeignKey(Gender, on_delete=models.CASCADE, null=True)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.name
