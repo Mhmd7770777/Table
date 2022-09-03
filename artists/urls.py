6b19f2a5c116f1394545baecb3d0f0e51f1c9fb0
Joshua Rodriguez <joshua.david.rodriguez.medrano@gmail.com>
2022-09-03 10:44:57 -0600

    Add sorting

--- artists/urls.py
+++ artists/urls.py
@@ -4,5 +4,6 @@ from artists import views
 urlpatterns = [
     path("", views.ArtistsView.as_view()),
     path("filter/", views.ArtistFilterView.as_view()),
-    path("search", views.ArtistSearchView.as_view()),
+    path("search/", views.ArtistSearchView.as_view()),
+    path("sort/<type>", views.ArtistSortView.as_view()),
 ]
